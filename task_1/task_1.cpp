57ab5166074e2b77f57feb47b1b90dc199846834
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-04-26 15:00:38 +0300

    +1 задание

--- task_1/task_1.cpp
+++ task_1/task_1.cpp
@@ -4,10 +4,44 @@
 Point). Будет ли прямая, проходящая через эти точки, параллельна
 оси ординат или оси абсцисс?
 */
-// НЕ РАБОТАЕТ
+
 #include <iostream>
+#include <Windows.h>
+using std::cout;
+using std::endl;
+
+// глобальное объявление структуры "Point"
+struct Point
+{
+	// поля структуры, описывающие точку на плоскости
+	int x, y;
+};
+
+int IsParallelToOrdAxisOrAbscXxis(Point p1, Point p2)
+{
+	if (p1.y == p2.y) // Параллельна оси "X".
+		return 1;
+	else if (p1.x == p2.x) // Параллельна оси "Y".
+		return 2;
+	else
+		return 3; // Не параллельна ничему.
+}
 
 int main()
 {
+	SetConsoleOutputCP(1251);
 
+	Point p1{ 3, 2 };
+	Point p2{ 3, 4 };
+
+	int result = IsParallelToOrdAxisOrAbscXxis(p1, p2);
+
+	if (result == 1)
+		cout << "Параллельна оси \"X\"." << endl;
+	else if (result == 2)
+		cout << "Параллельна оси \"Y\"." << endl;
+	else if (result == 3)
+		cout << "Не параллельна ничему." << endl;
+
+	return 0;
 }
